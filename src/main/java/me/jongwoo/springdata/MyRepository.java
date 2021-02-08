8f69d2b40e82696ad1e17988f1aed58875c1dd21
limjongwoo <whddn528@naver.com>
2021-02-08 13:55:40 +0900

    feat:Common null check

--- src/main/java/me/jongwoo/springdata/MyRepository.java
+++ src/main/java/me/jongwoo/springdata/MyRepository.java
@@ -2,9 +2,11 @@ package me.jongwoo.springdata;
 
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.lang.NonNull;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 @NoRepositoryBean
 public interface MyRepository<T, Id extends Serializable> extends Repository<T, Id> {
@@ -14,4 +16,7 @@ public interface MyRepository<T, Id extends Serializable> extends Repository<T, 
    List<T> findAll();
 
    long count();
+
+   <E extends T> Optional<E> findById(Id id);
+
 }
--- src/test/java/me/jongwoo/springdata/CommentRepositoryTest.java
+++ src/test/java/me/jongwoo/springdata/CommentRepositoryTest.java
@@ -1,5 +1,6 @@
 package me.jongwoo.springdata;
 
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,6 +35,18 @@ public class CommentRepositoryTest {
         //Then
         assertThat(all.size()).isEqualTo(1);
         assertThat(count).isEqualTo(1);
+
+        //When
+        final Optional<Comment> byId = commentRepository.findById(100L);
+
+        //Then
+        assertThat(byId).isEmpty();
+        //final Comment comment1 = byId.orElseGet(Comment::new);
+
+        assertThrows(IllegalArgumentException.class, () ->{
+            byId.orElseThrow(IllegalArgumentException::new);
+        });
+
 
     }
 }
