281b66f926aa68fb8f3174cc8c2b8ceb91b0f2bc
limjongwoo <whddn528@naver.com>
2021-02-08 14:58:29 +0900

    feat:Common Query method study

--- src/main/java/me/jongwoo/springdata/CommentRepository.java
+++ src/main/java/me/jongwoo/springdata/CommentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 //@RepositoryDefinition(domainClass = Comment.class, idClass = Long.class)
 public interface CommentRepository extends MyRepository<Comment, Long>{
@@ -13,8 +14,9 @@ public interface CommentRepository extends MyRepository<Comment, Long>{
 //    List<Comment> findAll();
 
     //@Query(value = "SELECT c FROM Comment c")
-    List<Comment> findByCommentContains(String keyword);
+    List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountAsc(String keyword);
 
-    Page<Comment> findByLikeCountGreaterThanAndPost(int likeCount, Post post, Pageable pageable);
+    Page<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
 
+    //Stream<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
 }
--- src/test/java/me/jongwoo/springdata/CommentRepositoryTest.java
+++ src/test/java/me/jongwoo/springdata/CommentRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -24,17 +27,17 @@ public class CommentRepositoryTest {
     @Test
     public void crud(){
         //Given
-        Comment comment = new Comment();
-        comment.setComment("Hello Comment");
-        commentRepository.save(comment);
+        this.createComment(100, "spring data jpa");
+        this.createComment(55, "hibernate SPRING");
+        final PageRequest pageable = PageRequest.of(0, 10, Sort.by("likeCount").descending());
 
         //When
         final List<Comment> all = commentRepository.findAll();
         final long count = commentRepository.count();
 
         //Then
-        assertThat(all.size()).isEqualTo(1);
-        assertThat(count).isEqualTo(1);
+        assertThat(all.size()).isEqualTo(2);
+        assertThat(count).isEqualTo(2);
 
         //When
         final Optional<Comment> byId = commentRepository.findById(100L);
@@ -47,6 +50,24 @@ public class CommentRepositoryTest {
             byId.orElseThrow(IllegalArgumentException::new);
         });
 
+        //When
+        final List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseOrderByLikeCountAsc("spring");
+        //Then
+        assertThat(comments.size()).isEqualTo(2);
+        assertThat(comments).first().hasFieldOrPropertyWithValue("likeCount", 55);
 
+        //When
+        final Page<Comment> spring = commentRepository.findByCommentContainsIgnoreCase("spring", pageable);
+        //Then
+        assertThat(spring.getNumberOfElements()).isEqualTo(2);
+        assertThat(spring).first().hasFieldOrPropertyWithValue("likeCount", 100);
+
+    }
+
+    private void createComment(int likeCount, String comment) {
+        Comment newComment = new Comment();
+        newComment.setComment(comment);
+        newComment.setLikeCount(likeCount);
+        commentRepository.save(newComment);
     }
 }
