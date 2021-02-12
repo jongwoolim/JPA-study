dfca8742f1263af750fcc3f543cc6ff08c972847
limjongwoo <whddn528@naver.com>
2021-02-12 16:25:32 +0900

    feat:스프링 데이터 JPA(save()메서드)

--- src/test/java/me/jongwoo/springdata/PostRepositoryTest.java
+++ src/test/java/me/jongwoo/springdata/PostRepositoryTest.java
@@ -14,6 +14,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
@@ -32,6 +34,34 @@ public class PostRepositoryTest {
     @Autowired
     ApplicationContext applicationContext;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    public void save(){
+        Post post = new Post();
+        post.setTitle("jpa");
+        final Post savedPost = postRepository.save(post); // persist
+
+        assertThat(entityManager.contains(post)).isTrue();
+        assertThat(entityManager.contains(savedPost)).isTrue();
+        assertThat(savedPost == post);
+
+        Post postUpdate = new Post();
+        postUpdate.setId(post.getId());
+        postUpdate.setTitle("hibernate");
+
+        //merge()메소드에 넘긴 그 엔티티의 복사본을 만들고 그 복사본을 Persistent 상태로 변경하고 복사본 반환
+        final Post updatedPost = postRepository.save(postUpdate);// merge update 쿼리
+
+        assertThat(entityManager.contains(updatedPost)).isTrue();
+        assertThat(entityManager.contains(postUpdate)).isFalse();
+        assertThat(postUpdate == updatedPost);
+
+        final List<Post> all = postRepository.findAll();
+        assertThat(all.size()).isEqualTo(1);
+
+    }
 
     @Test
     public void querydslTest(){
